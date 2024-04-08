9501eda3e471079f9a14e13fe2ccf0825aa92e9d
Michael Campbell <rainbow@portal2d.xyz>
2024-04-07 20:04:52 -0400

    refactor next level advance to work in build

--- World/NextLevelAdvance.gd
+++ World/NextLevelAdvance.gd
@@ -1,17 +1,14 @@
 extends Area2D
 
 const worlds_path := "res://World/Worlds/"
-@export var win_screen: PackedScene
+@export var next_scene: PackedScene
 
 @onready var world_id := int(Get.ancestor_of_type(get_parent(), World).scene_file_path.split("/")[-1].split(".")[0])
 
 func _on_body_entered(body):
     if body is Player:
         var filename = worlds_path + str(world_id + 1) + ".tscn"
-        if FileAccess.file_exists(filename):
-            get_tree().change_scene_to_file.call_deferred(filename)
-        else:
-            get_tree().change_scene_to_packed.call_deferred(win_screen)
+        get_tree().change_scene_to_packed.call_deferred(next_scene)
 
 func _ready():
     print(
